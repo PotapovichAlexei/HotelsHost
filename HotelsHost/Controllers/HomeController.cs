d3b6b6fcd46d2670836d715d222c353a9dd0de71
Artem <tema5286@gmail.com>
2021-12-08 22:43:39 +0300

    Add project files.

---
+++ HotelsHost/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelsHost.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content(User.Identity.Name);
+        }
+    }
+}
---
+++ HotelsHost/Data/HotelsHostContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelsHost.Models;
+
+namespace HotelsHost.Data
+{
+    public class HotelsHostContext : DbContext
+    {
+        public HotelsHostContext (DbContextOptions<HotelsHostContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HotelsHost.Models.Hotel> Hotels { get; set; }
+
+        public DbSet<HotelsHost.Models.User> Users { get; set; }
+    }
+}
